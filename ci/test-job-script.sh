199798aee61eb3ea87c8ee159855bb667b827261
zosmac <zosmac@github.com>
2025-06-22 19:44:18 -0400

    set all PATH variables for tests

--- ci/test-job-script.sh
+++ ci/test-job-script.sh
@@ -29,7 +29,20 @@ elif [ "${ID_LIKE:-}" = "debian" ]; then
   export TCLLIBPATH=/usr/lib/tcltk/graphviz/tcl
 elif [ "${build_system}" = "autotools" ]; then
   if [ "${ID}" = "Darwin" ]; then
-    export PATH=$PATH:$( cat /etc/paths.d/graphviz )
+    if [ -a /etc/paths.d/graphviz ]; then
+      PREFIX=$(cat /etc/paths.d/graphviz)
+      PREFIX=${PREFIX%/bin}
+    else
+      PREFIX=/usr/local
+    fi
+    export PATH=$PATH:${PREFIX}/bin \
+      C_INCLUDE_PATH=${PREFIX}/include \
+      DYLD_LIBRARY_PATH=${PREFIX}/lib \
+      LIBRARY_PATH=${PREFIX}/lib \
+      PYTHONPATH=${PREFIX}/lib/graphviz/python3 \
+      TCLLIBPATH=${PREFIX}/lib/graphviz/tcl \
+      PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig \
+      graphviz_ROOT=${PREFIX}
   fi
 fi
 
