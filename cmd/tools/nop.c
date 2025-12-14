c295aea972fd0c8563b4d8a19926e17902e19aef
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-12-14 09:05:52 -0800

    clang-format nop.c

--- cmd/tools/nop.c
+++ cmd/tools/nop.c
@@ -4,7 +4,7 @@
  */
 
 /*************************************************************************
- * Copyright (c) 2011 AT&T Intellectual Property 
+ * Copyright (c) 2011 AT&T Intellectual Property
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,55 +30,53 @@ static const char useString[] = "Usage: nop [-p?] <files>\n\
   -? - print usage\n\
 If no files are specified, stdin is used";
 
-static void usage(int v)
-{
-    puts(useString);
-    graphviz_exit(v);
+static void usage(int v) {
+  puts(useString);
+  graphviz_exit(v);
 }
 
-static void init(int argc, char *argv[])
-{
-    int c;
+static void init(int argc, char *argv[]) {
+  int c;
 
-    opterr = 0;
-    while ((c = getopt(argc, argv, "p?")) != -1) {
-	switch (c) {
-	case 'p':
-	    chkOnly = true;
-	    break;
-	case '?':
-	    if (optopt == '\0' || optopt == '?')
-		usage(EXIT_SUCCESS);
-	    else {
-		fprintf(stderr, "nop: option -%c unrecognized\n",
-			optopt);
-		usage(EXIT_FAILURE);
-	    }
-	    break;
-	default:
-	    fprintf(stderr, "nop: unexpected error\n");
-	    graphviz_exit(EXIT_FAILURE);
-	}
+  opterr = 0;
+  while ((c = getopt(argc, argv, "p?")) != -1) {
+    switch (c) {
+    case 'p':
+      chkOnly = true;
+      break;
+    case '?':
+      if (optopt == '\0' || optopt == '?')
+        usage(EXIT_SUCCESS);
+      else {
+        fprintf(stderr, "nop: option -%c unrecognized\n", optopt);
+        usage(EXIT_FAILURE);
+      }
+      break;
+    default:
+      fprintf(stderr, "nop: unexpected error\n");
+      graphviz_exit(EXIT_FAILURE);
     }
-    argv += optind;
-    argc -= optind;
+  }
+  argv += optind;
+  argc -= optind;
 
-    if (argc)
-	Files = argv;
+  if (argc)
+    Files = argv;
 }
 
-int main(int argc, char **argv)
-{
-    Agraph_t *g;
-    ingraph_state ig;
+int main(int argc, char **argv) {
+  Agraph_t *g;
+  ingraph_state ig;
 
-    init(argc, argv);
-    newIngraph(&ig, Files);
+  init(argc, argv);
+  newIngraph(&ig, Files);
 
-    while ((g = nextGraph(&ig)) != 0) {
-	if (!chkOnly) agwrite(g, stdout);
-	agclose(g);
-    }
+  while ((g = nextGraph(&ig)) != 0) {
+    if (!chkOnly)
+      agwrite(g, stdout);
+    agclose(g);
+  }
 
-    graphviz_exit(ig.errors != 0 || agerrors() != 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+  graphviz_exit(ig.errors != 0 || agerrors() != 0 ? EXIT_FAILURE
+                                                  : EXIT_SUCCESS);
 }
